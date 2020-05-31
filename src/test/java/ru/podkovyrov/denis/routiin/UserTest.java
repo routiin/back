03764593f53dc183dc451b9e1e614bbfcd0fc6ea
Podkovyrov Denis <denis.podkovyrov@gmail.com>
2020-05-31 19:52:27 +0300

    feat: clear testDatabase after test's

--- src/test/java/ru/podkovyrov/denis/routiin/UserTest.java
+++ src/test/java/ru/podkovyrov/denis/routiin/UserTest.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Sql(value = {"/create-user.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/create-user-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UserTest {
     @Autowired
     private MockMvc mockMvc;
