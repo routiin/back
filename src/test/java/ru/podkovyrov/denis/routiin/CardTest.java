a39183940224cc39c816659389fdb12f5120c743
Podkovyrov Denis <denis.podkovyrov@gmail.com>
2020-06-08 12:15:03 +0600

    fix: change authorizedRedirectUris

--- src/test/java/ru/podkovyrov/denis/routiin/CardTest.java
+++ src/test/java/ru/podkovyrov/denis/routiin/CardTest.java
@@ -5,22 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.podkovyrov.denis.routiin.entities.User;
 import ru.podkovyrov.denis.routiin.payloads.DayInterval;
-import ru.podkovyrov.denis.routiin.payloads.UserPostRequest;
 import ru.podkovyrov.denis.routiin.security.TokenProvider;
 import ru.podkovyrov.denis.routiin.service.UserService;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static ru.podkovyrov.denis.routiin.controller.api.v1.ControllerConstants.API_VERSION;
 import static ru.podkovyrov.denis.routiin.util.Convert.asJsonString;
 
 @SpringBootTest
